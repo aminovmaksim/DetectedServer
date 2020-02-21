cbfcaccb1b51631b5440f56c5d30e135ae56c0ab
Max Aminov <aminovmaksim@gmail.com>
2020-02-21 18:17:12 +0300

    Fix bugs

--- src/main/java/com/devian/detected/controllers/TaskController.java
+++ src/main/java/com/devian/detected/controllers/TaskController.java
@@ -11,17 +11,15 @@ import com.devian.detected.utils.NetworkManager;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
-@RestController(value = "/api")
+@RestController
+@RequestMapping("/api")
 @SuppressWarnings("unused")
 public class TaskController {
 

ccb7a0ac16db9e45c7ce615791d96b43218d3be0
Max Aminov <aminovmaksim@gmail.com>
2020-02-25 17:25:43 +0300

    Add confidential policy

---
+++ src/main/java/com/devian/detected/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.devian.detected.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/policy")
+    public String getPolitics() {
+        return "policy.html";
+    }
+}
